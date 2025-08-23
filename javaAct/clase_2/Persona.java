9f16d900a37dafaad724590ec31f6e7aa3e23704
LivaMolina <ivamolina1980@gmail.com>
2025-08-23 20:19:13 -0300

    Agrego ejercicio break-continue clase1 y Crearclases clase2 en Java

---
+++ javaAct/clase_2/Persona.java
@@ -0,0 +1,20 @@
+/* Ejercicio 3: En este ejercicio vamos a empezar a crear clases
+
+Una clase es una plantilla en donde vamos a trabjar
+La clase va a poseer un nombre, va a tener atributos y métodos
+los cuales extraen la información del mundo real
+Ej. Una clase puede tener como atributos, el nombre, telefono, dni, domicilio
+de una persona, también puede  p. ej. métodos para tomar la edad de la persona
+En resumen: Una clase es una plantilla con atributos y métodos a partir de la cual 
+vamos a poder crear objetos. Un Objeto es una instancia de una clase.
+En los objetos vamos a asignar valores, en la clase vamos a definir atributos y
+métodos de manera general, es como el molde para crear objetos.
+*/
+package Clase2Ejercicio3ClasesObjetos;//el package es como una carpeta donde se clasfican las clases
+
+public class Persona {//esta es nuestra plantilla llamada persona
+//public: la clase debe ser pública para poder acceder desde otros archivos
+//la palabra reservada Class, se refiere a que es una clase 
+//el nombre se debe colocar con la escritura PascalCase
+//luego van las llaves y luego se coloca una identación que es donde va el código    
+}
