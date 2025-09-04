d6078fbcad112e02a5f540bdf1f865b9b1490034
GeraldineZoe <garnizg@gmail.com>
2025-09-04 20:58:16 -0300

    JavaScript: Subido punto 1.1 Introducción a Funciones

---
+++ JavaScript/clase_3/1-1IntroducciónFunciones.js
@@ -0,0 +1,9 @@
+// 1.1 Introducción a Funciones JavaScript
+numero(77, 77); //Esto se conoce como HOSTING
+
+function numero(a, b){
+    console.log("Sumamos: "+ (a + b));
+}
+
+//Llamando la función 
+numero( 9, 3);
