59e964d79c1094b2357d472bd42faffc7223c93e
Ruben M <rubenmarchisio@gmail.com>
2025-10-04 09:24:09 -0300

    cargo la clase 6 primera parte de java lo que es practicas y tamas

---
+++ javaAct/clase_4/Personaa.java
@@ -0,0 +1,12 @@
+
+package javaa.clase_4;
+
+public class Personaa {
+    public String nombre;
+    public String apellido;
+
+    public void obtenerInformacion(){
+        System.out.println("Nombre: " + nombre);
+        System.out.println("Apellido: " + apellido);
+    }
+}
