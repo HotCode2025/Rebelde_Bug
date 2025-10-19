9629831ac3785544985c2636aedcb3f8af6b11b2
LivaMolina <ivamolina1980@gmail.com>
2025-10-18 22:41:04 -0300

    Agrego archivo Clase 4 de Funciones en Python

---
+++ Python/clase_4/Clase4Funciones.py
@@ -0,0 +1,9 @@
+# Comenzamos con funciones
+# mi_funcion() # No se puede llamar antes de definir a una función
+# Definimos una función
+def mi_funcion():
+    print('Saludos a todos los alumnos de la tecnicatura')
+
+mi_funcion()# Estamos llamando a la función
+mi_funcion()# Se puede llamar a una función N cantidad de veces
+mi_funcion()
