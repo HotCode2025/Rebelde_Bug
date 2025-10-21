01e820db104108a15623be3043d412f1f1dd651b
Melina Gallo <melinadenisegallo@gmail.com>
2025-10-21 17:04:27 -0300

    Agrego clase 6.6 Funciones Paso Argumentos

---
+++ Python/clase_5/6.6_FuncionesPasoArgumentos.py
@@ -0,0 +1,8 @@
+#Paso de Argumentos
+def mi_Funcion2(name, lastName):
+    print('Saludos a todos los que me ven a través del canal de YouTube')
+    print(f'Nombre: {name}, Apellido: {lastName}')
+mi_Funcion2('Jorge', 'Lucero')
+mi_Funcion2('Rubén', 'Marchisio')
+mi_Funcion2('Camila', 'Scheurer')
+mi_Funcion2('Melina', 'Gallo')
