d220999247c5cb3431b0b6db849f067a77adfbcd
Melina Gallo <melinadenisegallo@gmail.com>
2025-10-21 17:12:39 -0300

    Modifico tarea 6.7 y guardo cambios

--- Python/clase_5/6.7_FuncionesPalabraReturn.py
+++ Python/clase_5/6.7_FuncionesPalabraReturn.py
@@ -4,5 +4,6 @@
 def sumar(a, b):
     return a + b
 
-resultado = sumar(78, 22)
-print(f'El resultado de la suma es: {resultado}')
+# resultado = sumar(78, 22)
+# print(f'El resultado de la suma es: {resultado}')
+print(f'El resultado de la suma es: {sumar(55, 45)}')
