91fa0264e34ed0e3f4b20eea62e0153522257a5c
Melina Gallo <melinadenisegallo@gmail.com>
2025-10-22 00:42:07 -0300

    Agrego Clase Aritmetica video 9.4

---
+++ Python/clase_7/9.4_ClaseAritmeticaRestaMultiplicacionDivisio.py
@@ -0,0 +1,21 @@
+def __init__(self, operandoA, operandoB):
+    self.operandoA = operandoA
+    self.operandoB = operandoB
+
+    # Método para sumar
+
+
+def sumar(self):
+    return self.operandoA + self.operandoB
+
+
+def resta(self):
+    return self.operandoA - self.operandoB
+
+
+def multiplicar(self):
+    return self.operandoA * self.operandoB
+
+
+def dividir(self):
+    return self.operandoA / self.operandoB
