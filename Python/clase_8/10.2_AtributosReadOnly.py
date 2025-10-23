50f1d1652601ddc8f7093703c679395467a43a95
Melina Gallo <melinadenisegallo@gmail.com>
2025-10-23 00:42:27 -0300

    10.2 Atributos Read Only

--- Python/clase_8/10.2_AtributosReadOnly.py
+++ Python/clase_8/10.2_AtributosReadOnly.py
@@ -21,24 +21,27 @@ print(persona1.nombre) #Llamamos al método getter
 
 # Tarea método set y get con apellido y edad
 
-@property
-def apellido(self):
-    return self._apellido
+#@property
+#def apellido(self):
+#    return self._apellido
 
 
-@apellido.setter
-def apellido(self, apellido):
-    print('Estamos utilizando el método SETTER de apellido')
-    self._apellido = apellido
+#@apellido.setter
+#def apellido(self, apellido):
+#    print('Estamos utilizando el método SETTER de apellido')
+#    self._apellido = apellido
 
-@property
-def edad(self):
-    return self._edad
+#@property
+#def edad(self):
+#    return self._edad
 
-@edad.setter
-def edad(self, edad):
-    print('Estamos utilizando el método SETTER de edad')
-    self._edad = edad
+#@edad.setter
+#def edad(self, edad):
+#    print('Estamos utilizando el método SETTER de edad')
+#    self._edad = edad
 persona1.nombre = 'Juan Pedro' #Llamamos el metodo setter
 print(persona1.nombre) # metodo getter
 print(persona1.mostrar_detalles()) #Llamamos al metodo mostrar detalles
+#Atributo read only sería la edad porque no tiene el método set
+persona1._edad = '40'
+print(persona1.edad)
