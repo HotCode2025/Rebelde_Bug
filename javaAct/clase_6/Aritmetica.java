08e7a4ab3cb56d8842eced19bd7d467354e2809d
micaela <aleacimoyamara@gmail.com>
2025-10-24 06:20:44 -0300

    Añado videos PruebaAritmetica y Aritmetica con usos del This y Encapsulamiento

---
+++ javaAct/clase_6/Aritmetica.java
@@ -0,0 +1,39 @@
+
+package Operaciones;
+
+
+public class Aritmetica {
+    //Atributos de la clase
+    int a;
+    int b;
+    
+    //El constructor es un metodo especial
+    public Aritmetica(){ //constructor 1
+        System.out.println("Se está ejecutando este constructor numero uno");
+    }
+    //estamos viendo lo que se llama sobrecarga de constructores
+    public Aritmetica(int a, int b){ //constructor 2
+        this.a = a;
+        this.b = b;
+        System.out.println("Se está ejecutando el constructor numero dos");
+    }
+    //Metodo
+    public void sumarNumeros(){
+        int resultado = a + b;
+        System.out.println("resultado = " + resultado);
+        
+    }
+    public int sumarConRetorno(){
+        //int resultado = a + b;
+        return this.a + this.b;
+    }
+    public int sumarConArgumentos(int a, int b){
+        this.a = a; //el argumento a se asigna al atributo this.a
+        this.b = b;
+        //return a + b;
+        return this.sumarConRetorno();
+    }
+}
+    
+
+
