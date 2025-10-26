9059ec90970d9bb4423bcd1add1dad5605eb7315
Milasch23 <kamyxsch@gmail.com>
2025-10-25 21:36:51 -0300

    Carga Carpeta Clase 9 y video 9.1

---
+++ JavaScript/clase_9/video9-1.js
@@ -0,0 +1,5 @@
+// 9.1 Introducción a lo que es la herencia en JavaScript
+// La herencia es un concepto fundamental en la programación orientada a objetos que permite a una clase (subclase) heredar propiedades 
+// y métodos de otra clase (superclase). En JavaScript, la herencia se implementa principalmente a través de prototipos.
+// (Se adjunta ejemplo en un archivo .uxf)
+// En el ejemplo anterior, las clases "Empleado" y "Cliente" heredan de la clase "Persona" utilizando la palabra clave "extends".
