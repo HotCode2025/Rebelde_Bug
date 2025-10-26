ac6c35e91b1070c8313229ce28d782d232bc463a
Milasch23 <kamyxsch@gmail.com>
2025-10-26 18:19:36 -0300

    video 9.5 - Cargando todo en una misma plantilla

---
+++ JavaScript/clase_9/PruebaPersonas.js
@@ -0,0 +1,105 @@
+//9.5 Cargando todo en una misma plantilla
+
+class Persona {
+    static contadorPersonas = 0;
+    constructor(nombre, apellido, edad){
+        this._idPersona = ++Persona.contadorPersonas;
+        this._nombre = nombre;
+        this._apellido = apellido;
+        this._edad = edad;
+    }
+
+    get idPersona(){
+        return this._idPersona;
+    }
+
+    get nombre(){
+        return this._nombre;
+    }
+
+    set nombre(nombre){
+        this._nombre = nombre;
+    }
+
+    get apellido(){
+        return this._apellido;
+    }
+
+    set apellido(apellido){
+        this._apellido = apellido;
+    }
+
+    get edad(){
+        return this._edad;
+    }
+
+    set edad(edad){
+        this._edad = edad;
+    }
+
+    toString(){
+        return `ID: ${this._idPersona}, 
+                Nombre: ${this._nombre}, 
+                Apellido: ${this._apellido}, 
+                Edad: ${this._edad}`;
+    }
+
+}
+
+class Empleado extends Persona{
+
+    static contadorEmpleados = 0;
+
+    constructor(nombre, apellido, edad, salario){
+        super(nombre, apellido, edad);
+        this._idEmpleado = ++Empleado.contadorEmpleados;
+        this._salario = salario;
+    }
+
+    get idEmpleado(){
+        return this._idEmpleado;
+    }
+
+    get salario(){
+        return this._salario;
+    }
+
+    set salario(salario){
+        this._salario = salario;
+    }
+
+    toString(){
+        return `${super.toString()},
+                ID Empleado: ${this._idEmpleado},
+                Salario: ${this._salario}`;
+    }
+}
+
+class Cliente extends Persona{
+
+    static contadorClientes = 0;
+
+    constructor(nombre, apellido, edad, fechaRegistro){
+        super(nombre, apellido, edad);
+        this._idCliente = ++Cliente.contadorClientes;
+        this._fechaRegistro = fechaRegistro;
+    }
+
+    get idCliente(){
+        return this._idCliente;
+    }
+
+    get fechaRegistro(){
+        return this._fechaRegistro;
+    }
+
+    set fechaRegistro(fechaRegistro){
+        this._fechaRegistro = fechaRegistro;
+    }
+
+    toString(){
+        return `${super.toString()},
+                ID Cliente: ${this._idCliente},
+                Fecha de Registro: ${this._fechaRegistro}`;
+    }
+}
